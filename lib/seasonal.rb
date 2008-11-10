d2b88cdd4839534c6b3fe6753a5183cf6e3dbdeb
Matthew M. Boedicker <matthewm@boedicker.org>
2008-11-10 02:49:51 -0500

    simplify interface

--- lib/seasonal.rb
+++ lib/seasonal.rb
@@ -8,46 +8,46 @@ module Seasonal
   class Event
     attr_accessor :payload
     attr_accessor :zone
-    attr_accessor :start_date
-    attr_accessor :start_time
-    attr_accessor :end_date
-    attr_accessor :end_time
+    attr_accessor :start
+    attr_accessor :ennd
 
     def initialize(payload, zone, options={})
       @payload = payload
       @zone = zone
-      @start_date = options[:start_date]
-      @start_time = options[:start_time] || '00:00:00'
-      @end_date = options[:end_date] || options[:start_date]
+      @start = options[:start]
+      @ennd = options[:end]
+    end
 
-      if options[:end_time].nil?
-        if options[:start_time].nil?
-          @end_time = '23:59:59'
-        else
-          if options[:end_date].nil?
-            @end_time = options[:start_time]
-          else
-            @end_time = '23:59:59'
-          end
-        end
-      else
-        @end_time = options[:end_time]
+    def start_utc
+      unless start.nil?
+        tz = TZInfo::Timezone.get(zone)
+        tz.local_to_utc(Time.parse(start))
       end
     end
 
-    def start
-      tz = TZInfo::Timezone.get(zone)
-      tz.local_to_utc(Time.parse("#{start_date} #{start_time}"))
-    end
-
-    def ennd
-      tz = TZInfo::Timezone.get(zone)
-      tz.local_to_utc(Time.parse("#{end_date} #{end_time}"))
+    def end_utc
+      unless ennd.nil?
+        tz = TZInfo::Timezone.get(zone)
+        tz.local_to_utc(Time.parse(ennd))
+      end
     end
 
     def going_on?(test_time=Time.now)
-      # puts "#{start} - #{ennd}"
-      test_time >= start and test_time <= ennd
+      puts "#{start_utc} - #{end_utc}"
+      if start_utc.nil?
+        if end_utc.nil?
+          result = false
+        else
+          result = test_time <= end_utc
+        end
+      else
+        if end_utc.nil?
+          result = test_time >= start_utc
+        else
+          result = (test_time >= start_utc and test_time <= end_utc)
+        end
+      end
+      result
     end
 
   end
--- test/seasonal_test.rb
+++ test/seasonal_test.rb
@@ -6,103 +6,52 @@ require 'test/unit'
 
 class SeasonalTest < Test::Unit::TestCase
 
-  def test_start_date
-    e = Seasonal::Event.new(nil, 'America/New_York', :start_date => 'jan 1')
+  def test_none
+    e = Seasonal::Event.new(nil, 'America/New_York')
 
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year - 1, 12, 30, 23, 59, 59)))
-
-    assert(e.going_on?(Time.utc(Time.now.utc.year, 1, 1, 17, 0, 0)))
-
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 1, 2, 5, 0, 0)))
+    assert_equal(false, e.going_on?)
   end
 
-  def test_start_date_end_time
-    e = Seasonal::Event.new(nil, 'America/New_York', :start_date => 'feb 22',
-      :end_time => '5:00pm')
+  def test_end
+    es = 'mar 17'
+    ennd = Time.parse(es).utc
 
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 2, 22, 4, 59, 59)))
+    e = Seasonal::Event.new(nil, 'America/New_York', :end => es)
 
-    assert(e.going_on?(Time.utc(Time.now.utc.year, 2, 22, 13, 0, 0)))
-
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 2, 22, 22, 0, 1)))
+    assert(e.going_on?(ennd - 1))
+    assert(e.going_on?(ennd))
+    assert_equal(false, e.going_on?(ennd + 1))
   end
 
-  def test_start_date_end_date
-    e = Seasonal::Event.new(nil, 'America/New_York',
-      :start_date => 'mar 3 2003', :end_date => 'mar 5 2003')
+  def test_start
+    ss = 'jan 2'
+    start = Time.parse(ss).utc
 
-    assert_equal(false, e.going_on?(Time.utc(2003, 3, 3, 4, 59, 59)))
+    e = Seasonal::Event.new(nil, 'America/New_York', :start => ss)
 
-    assert(e.going_on?(Time.utc(2003, 3, 4, 0, 0, 0)))
-
-    assert_equal(false, e.going_on?(Time.utc(2003, 3, 6, 5, 0, 0)))
+    assert_equal(false, e.going_on?(start - 1))
+    assert(e.going_on?(start))
+    assert(e.going_on?(start + 1))
   end
 
-  def test_start_date_end_date_end_time
-    e = Seasonal::Event.new(nil, 'America/New_York',
-      :start_date => 'apr 20', :end_date => 'may 1', :end_time => '00:00')
+  def test_start_end
+    ss = 'aug 29'
+    es = 'sep 1'
+    start = Time.parse(ss).utc
+    ennd = Time.parse(es).utc
 
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 4, 20, 3, 59, 59)))
+    e = Seasonal::Event.new(nil, 'America/New_York', :start => ss, :end => es)
 
-    assert(e.going_on?(Time.utc(Time.now.utc.year, 5, 1, 0, 0, 0)))
+    assert_equal(false, e.going_on?(start - 1))
+    if start <= ennd
+      assert(e.going_on?(start))
+      assert(e.going_on?(ennd))
+    else
+      assert_equal(false, e.going_on?(start))
+      assert_equal(false, e.going_on?(ennd))
+    end
 
-    assert_equal(false, e.going_on?(Time.utc(Time.now.utc.year, 5, 2, 0, 0, 0)))
-  end
-
-  def test_start_date_start_time
-    e = Seasonal::Event.new(nil, 'America/New_York', :start_date => 'may 10',
-      :start_time => '10:00am')
-
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 5, 10, 13, 59, 59)))
-
-    assert(e.going_on?(Time.utc(Time.now.utc.year, 5, 10, 14, 0, 0)))
-
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 5, 10, 14, 0, 1)))
-  end
-
-  def test_start_date_start_time_end_time
-    e = Seasonal::Event.new(nil, 'America/New_York', :start_date => 'june 22',
-      :start_time => '10:00am', :end_time => '10:30am')
-
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 6, 22, 13, 59, 59)))
-
-    assert(e.going_on?(Time.utc(Time.now.utc.year, 6, 22, 14, 15, 0)))
-
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 6, 22, 14, 30, 1)))
-  end
-
-  def test_start_date_start_time_end_date
-    e = Seasonal::Event.new(nil, 'America/New_York', :start_date => 'july 4',
-      :start_time => '11:00pm', :end_date => 'jul 5')
-
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 7, 5, 2, 59, 59)))
-
-    assert(e.going_on?(Time.utc(Time.now.utc.year, 7, 5, 15, 0, 0)))
-
-    assert_equal(false, e.going_on?(Time.utc(Time.now.utc.year, 7, 6, 4, 0, 0)))
-  end
-
-  def test_start_date_start_time_end_date_end_time
-    e = Seasonal::Event.new(nil, 'America/New_York', :start_date => 'aug 19',
-      :start_time => '1:00pm', :end_date => 'aug 21', :end_time => '2:00pm')
-
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 8, 19, 16, 59, 59)))
-
-    assert(e.going_on?(Time.utc(Time.now.utc.year, 8, 20, 15, 0, 0)))
-
-    assert_equal(false,
-      e.going_on?(Time.utc(Time.now.utc.year, 8, 21, 18, 0, 1)))
+    assert_equal(false, e.going_on?(ennd + 1))
   end
 
 end
