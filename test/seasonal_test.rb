103659db01a7b7ea10df4ad642dbb14f7d57bde7
Matthew M. Boedicker <matthewm@boedicker.org>
2008-11-10 08:31:49 -0500

    better test names and order

--- test/seasonal_test.rb
+++ test/seasonal_test.rb
@@ -6,24 +6,13 @@ require 'test/unit'
 
 class SeasonalTest < Test::Unit::TestCase
 
-  def test_none
+  def test_event_none
     e = Seasonal::Event.new(nil, 'America/New_York')
 
     assert_equal(false, e.going_on?)
   end
 
-  def test_end
-    es = 'mar 17'
-    ennd = Time.parse(es).utc
-
-    e = Seasonal::Event.new(nil, 'America/New_York', :end => es)
-
-    assert(e.going_on?(ennd - 1))
-    assert(e.going_on?(ennd))
-    assert_equal(false, e.going_on?(ennd + 1))
-  end
-
-  def test_start
+  def test_event_start
     ss = 'jan 2'
     start = Time.parse(ss).utc
 
@@ -34,7 +23,18 @@ class SeasonalTest < Test::Unit::TestCase
     assert(e.going_on?(start + 1))
   end
 
-  def test_start_end
+  def test_event_end
+    es = 'mar 17'
+    ennd = Time.parse(es).utc
+
+    e = Seasonal::Event.new(nil, 'America/New_York', :end => es)
+
+    assert(e.going_on?(ennd - 1))
+    assert(e.going_on?(ennd))
+    assert_equal(false, e.going_on?(ennd + 1))
+  end
+
+  def test_event_start_end
     ss = 'aug 29'
     es = 'sep 1'
     start = Time.parse(ss).utc
@@ -43,15 +43,8 @@ class SeasonalTest < Test::Unit::TestCase
     e = Seasonal::Event.new(nil, 'America/New_York', :start => ss, :end => es)
 
     assert_equal(false, e.going_on?(start - 1))
-
-    if start <= ennd
-      assert(e.going_on?(start))
-      assert(e.going_on?(ennd))
-    else
-      assert_equal(false, e.going_on?(start))
-      assert_equal(false, e.going_on?(ennd))
-    end
-
+    assert(e.going_on?(start))
+    assert(e.going_on?(ennd))
     assert_equal(false, e.going_on?(ennd + 1))
   end
 
